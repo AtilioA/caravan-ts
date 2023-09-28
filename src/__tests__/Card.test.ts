6f3f8b5176f4f4024b17c48edd698ade749d8464
AtilioA <atiliogpg@protonmail.com>
2023-09-28 10:03:08 -0300

    feat: add suit and value checks to `Card`

--- src/__tests__/Card.test.ts
+++ src/__tests__/Card.test.ts
@@ -1,3 +1,4 @@
+import { SUITS, VALUES } from "../constants/cardConstants";
 import { Card } from "../models/Card";
 
 describe('Card', () => {
@@ -5,5 +6,19 @@ describe('Card', () => {
         const card = new Card('Ace', 'Spades');
         expect(card.value).toBe('Ace');
         expect(card.suit).toBe('Spades');
+    });
+
+    it('should only allow valid values and suits', () => {
+        // Use SUITS and VALUES from cardConstants.ts, any other value should throw an error
+        expect(() => new Card('Ace', 'Spades')).not.toThrow();
+        expect(() => new Card('Ace', 'Spade')).toThrow();
+        expect(() => new Card('Aces', 'Spades')).toThrow();
+        expect(() => new Card('Aces', 'Spade')).toThrow();
+    });
+
+    it('should allow any value and suit from cardConstants', () => {
+        // Use random values from SUITS and VALUES from cardConstants.ts
+        const card = new Card(VALUES[Math.floor(Math.random() * VALUES.length)], SUITS[Math.floor(Math.random() * SUITS.length)]);
+        expect(card.value).toBeDefined();
     });
 });
--- src/models/Card.ts
+++ src/models/Card.ts
@@ -1,6 +1,11 @@
+import { SUITS, VALUES } from '../constants/cardConstants';
 import { ICard } from './interfaces/ICard';
 
 export class Card implements ICard {
-  constructor(public value: string, public suit: string) {}
-  // leave methods unimplemented for now
+  constructor(public value: string, public suit: string) {
+    // Check if the value and suit are valid
+    if (!VALUES.includes(value) || !SUITS.includes(suit)) {
+      throw new Error('Invalid card value or suit.');
+    }
+  }
 }
