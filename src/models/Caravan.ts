16257c92d497ca10d8658d110fdc071129d8b536
AtilioA <atiliogpg@protonmail.com>
2023-09-29 12:13:49 -0300

    feat: add basic functions for a `Caravan`

--- src/models/Caravan.ts
+++ src/models/Caravan.ts
@@ -1,5 +1,117 @@
+import { CardValues, ValueMapping } from '../constants/cardConstants';
+import { Direction } from '../enums/directions';
 import { ICaravan } from './interfaces/ICaravan';
+import { ICard } from './interfaces/ICard';
 
 export class Caravan implements ICaravan {
-  // leave methods unimplemented for now
+  constructor(public cards: ICard[] = [], public direction: Direction | null = null, public suit: string | null = null, public bid: number = 0) {}
+
+  private _isValueInDirection(value: CardValues): boolean {
+    const lastCardValue = ValueMapping[this.cards[this.cards.length - 1].value]
+
+    switch (this.direction) {
+      case Direction.ASCENDING:
+        return ValueMapping[value] > lastCardValue;
+      case Direction.DESCENDING:
+        return ValueMapping[value] < lastCardValue;
+      default:
+        // NOTE: throw an error since the direction should always be defined at this point?
+        return false;
+    }
+  }
+
+  canAddCard(card: ICard): boolean {
+    // If the caravan is empty and the card is a face card, return false.
+    if (this.cards.length === 0) {
+      if (card.isFaceCard()) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    if (card.isFaceCard()) {
+      return true;
+    }
+
+    // If the caravan has a defined suit and the card's suit matches the caravan's suit, return true.
+    if (this?.suit === card.suit) {
+        return true;
+    }
+
+    if (this.direction) { // Check if there is a defined direction
+      return this._isValueInDirection(card.value);
+    }
+    return true;
+  }
+
+
+  addCard(card: ICard): void {
+    if (!this.canAddCard(card)) {
+      throw new Error("Cannot add this card to the caravan.");
+    }
+    // For the first card, set the suit of the Caravan.
+    if (this.cards.length === 0) {
+      this.suit = card.suit;
+    }
+
+    // Set or validate the direction with the second card.
+    if (this.cards.length === 1) {
+      const lastCardValue = ValueMapping[this.cards[0].value];
+      if (ValueMapping[card.value] > lastCardValue) {
+        this.direction = Direction.ASCENDING;
+      } else if (ValueMapping[card.value] < lastCardValue) {
+        this.direction = Direction.DESCENDING;
+      } else {
+        throw new Error("Equal card values are not allowed.");
+      }
+    }
+
+    // Change the suit when a card of a different suit is added.
+    if (card.suit !== this.suit) {
+      this.suit = card.suit;
+    }
+
+    // Update bid for non-face cards.
+    if (!card.isFaceCard()) {
+      this.bid += ValueMapping[card.value];
+    } else {
+      // Because of Joker, we might need to handle this within the game logic (we need to view every other caravan in order to determine the outcome of playing a Joker).
+      /*
+      if (card.value === 'King') {
+        this.bid += ValueMapping[this.cards[this.cards.length - 1].value];
+      } else if (card.value === 'Jack') {
+        // Remove all cards of the same suit and recalculate the bid.
+        this.cards = this.cards.filter(c => c.suit !== card.suit);
+        this.bid = this.cards.reduce((acc, c) => acc + ValueMapping[c.value], 0);
+      }
+      */
+      // Change suit and nullify direction for Queen.
+      if (card.value === 'Queen') {
+        this.suit = card.suit;
+        this.direction = null;
+      }
+    }
+
+    // Finally, add the card to the caravan.
+    this.cards.push(card);
+  }
+
+  // Might not be needed (we'd just change the bid when cards are played)
+  computeValue(): number {
+    // implementation here
+    throw new Error("Method not implemented.");
+  }
+
+  // Return the cards in the caravan and reset the caravan.
+  disband(): ICard[] {
+    const cards = this.cards;
+
+    this.cards = [];
+    this.direction = null;
+    this.suit = null;
+    this.bid = 0;
+
+    return cards;
+  }
 }
--- src/models/interfaces/ICaravan.ts
+++ src/models/interfaces/ICaravan.ts
@@ -1,3 +1,19 @@
+import { EnumSuits } from "../../enums/cards";
+import { Direction } from "../../enums/directions";
+import { ICard } from "./ICard";
+
 export interface ICaravan {
-  // properties and methods that a caravan should have
+  // TODO: change to EnumSuits
+  cards: ICard[];
+  direction: Direction | null;
+  suit: string | null;
+  bid: number;
+
+  addCard(card: ICard): void;
+  canAddCard(card: ICard): boolean;
+
+  // Might not be needed (we'd just change the bid when cards are played)
+  computeValue(): number;
+
+  disband(): ICard[];
 }
