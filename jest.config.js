ffe5b0fca0f22d2aa55e43b8f6c91e69562f5e1f
AtilioA <atiliogpg@protonmail.com>
2023-10-11 12:58:53 -0300

    ci: try to use proper args for codecov actions

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
   collectCoverage: false,
   coverageDirectory: 'coverage',
+  coverageReporters: ["json", "lcov", "text", "clover"],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}', '!src/**/*.d.ts', // Adjust this path if your source files are located in a different directory
   ],
