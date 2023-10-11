0d803f930c0250effe02145a9970d4d99a1944b9
AtilioA <atiliogpg@protonmail.com>
2023-10-11 12:20:54 -0300

    refactor: remove unused custom exception

--- src/exceptions/GameExceptions.ts
+++ src/exceptions/GameExceptions.ts
@@ -1,11 +1,3 @@
-export class CardNotInHandError extends Error {
-  constructor(message?: string) {
-      super(message);
-      Object.setPrototypeOf(this, new.target.prototype);
-      this.name = CardNotInHandError.name;
-  }
-}
-
 export class InvalidPlayError extends Error {
   constructor(message?: string) {
       super(message);
