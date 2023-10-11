da352a348ef65e71f39e120f27d2644b7789d7a0
AtilioA <atiliogpg@protonmail.com>
2023-10-11 13:06:24 -0300

    chore: don't collect coverage by default

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
-  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ["json", "lcov", "text", "clover"],
   collectCoverageFrom: [
