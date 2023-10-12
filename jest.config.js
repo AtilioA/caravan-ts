e13f9ad7dd605a10f57b3423197fd00ae14a9627
AtilioA <atiliogpg@protonmail.com>
2023-10-12 15:03:26 -0300

    chore: ignore build files when testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/__mocks__/'],
+  testPathIgnorePatterns: ['/node_modules/', '/__mocks__/', '/dist/'],
   coverageDirectory: 'coverage',
   coverageReporters: ["json", "lcov", "text", "clover"],
   collectCoverageFrom: [
-    'src/**/*.{ts,tsx}', '!src/**/*.d.ts', // Adjust this path if your source files are located in a different directory
+    'src/**/*.test.{ts,tsx}', '!src/**/*.test.d.ts',
   ],
 };
