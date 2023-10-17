0d558f23c4f5bb107ed6c9697ffdd1cba104b375
AtilioA <atiliogpg@protonmail.com>
2023-10-17 10:47:49 -0300

    refactor: simplify `RandomStrategy`'s `makeMove`

--- src/models/AI/RandomStrategy.ts
+++ src/models/AI/RandomStrategy.ts
@@ -4,8 +4,8 @@ import { GameAction, GameState } from '../interfaces/IGame';
 export class RandomStrategy implements AIStrategy {
   makeMove(gameState: GameState): GameAction {
     const possibleMoves = gameState.AI.generatePossibleMoves();
+
     // Randomly select one move from all possible moves
-    const randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-    return randomMove;
+    return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
   }
 }
