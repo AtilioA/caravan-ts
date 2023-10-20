d78f08f364f82408e7f1e2c25c7d997ee0e1f265
AtilioA <atiliogpg@protonmail.com>
2023-10-20 10:44:02 -0300

    test: add `generatePossibleMoves()` case where opening rounds have already started

--- src/__tests__/Player.test.ts
+++ src/__tests__/Player.test.ts
@@ -6,7 +6,7 @@ import { Card } from "../models/Card";
 import { Deck } from "../models/Deck";
 import { Player } from "../models/Player";
 import { generateCards } from "../utils/card";
-import { createMockCaravan, createMockPlayer } from "./__mocks__/mockFactories";
+import { createMockCaravan, createMockCard, createMockPlayer } from "./__mocks__/mockFactories";
 
 describe('Player', () => {
     it('should create a player with an empty set of cards', () => {
@@ -351,7 +351,7 @@ describe('Player', () => {
         }
     });
 
-    it('should be able to generate an array of possible actions for taking any actions in any caravan during the opening rounds', () => {
+    it('should be able to generate an array of possible actions for taking any actions in any caravan at the start of the opening rounds', () => {
         const player = createMockPlayer();
         player.hand = generateCards(8, true);
 
@@ -376,6 +376,81 @@ describe('Player', () => {
         for (const caravan of player.caravans) {
             for (const card of player.hand) {
                 if (caravan.canAddCard(card) && !card.isFaceCard()) {
+                    expect(possibleActions).toContainEqual({
+                        player,
+                        action: {
+                            type: "PLAY_CARD",
+                            card,
+                            target: caravan
+                        }
+                    });
+                }
+
+                // Should NOT have an action for attaching each face card in hand to each valued card in each caravan
+                for (const caravanCard of caravan.cards) {
+                    if (caravanCard.canAttachFaceCard(card)) {
+                        expect(possibleActions).not.toContainEqual({
+                            player,
+                            action: {
+                                type: "PLAY_CARD",
+                                card,
+                                target: caravanCard
+                            }
+                        });
+                    }
+                }
+                // Should NOT have an action for disbanding any caravan
+                if (player.canDisbandCaravan(caravan)) {
+                    expect(possibleActions).not.toContainEqual({
+                        player,
+                        action: {
+                            type: "DISBAND_CARAVAN",
+                            caravan
+                        }
+                    });
+                }
+
+                // Finally, should NOT have an action for discarding each card in hand (any card can be discarded)
+                for (const card of player.hand) {
+                    expect(possibleActions).not.toContainEqual({
+                        player,
+                        action: {
+                            type: "DISCARD_DRAW",
+                            card,
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+    it('should be able to generate an array of possible actions for taking any actions in any caravan during the opening rounds', () => {
+        const player = createMockPlayer();
+        player.hand = generateCards(8, true);
+
+        // Initialize Player's caravans
+        player.caravans = [createMockCaravan(), createMockCaravan(), createMockCaravan()];
+        player.caravans[1].addCard(createMockCard('Ace', 'Spades'));
+        player.caravans[0].addCard(createMockCard('10', 'Diamonds'));
+
+        // Generate possible Player actions given that it's an opening round
+        const possibleActions = player.generatePossibleMoves(true);
+
+        // Player has 8 cards, but can't discard any cards during the opening rounds
+        const discardableCards = 0
+        // Caravans can't be disbanded during the opening rounds
+        const disbandableCaravans = 0
+        // Player can play cards in hand to caravans
+        const playableCards = player.hand.filter(card => player.caravans.some(caravan => caravan.isEmpty() && caravan.canAddCard(card))).length;
+
+        // 0 + 0 + number of valued cards
+        expect(possibleActions.length).toBeGreaterThanOrEqual(discardableCards + disbandableCaravans + playableCards);
+
+        // Now, check for the actual actions
+        // The possibleActions array should have an action for each card in hand for each caravan
+        for (const caravan of player.caravans) {
+            for (const card of player.hand) {
+                if (caravan.isEmpty() && caravan.canAddCard(card) && !card.isFaceCard()) {
                     expect(possibleActions).toContainEqual({
                         player,
                         action: {
