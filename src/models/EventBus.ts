345781c081e8543f09cb18e2a6ba612ebd6e9dff
AtilioA <atiliogpg@protonmail.com>
2023-10-23 12:13:34 -0300

    feat: create EventBus to replace EventEmitter
    This commit introduces a new EventBus class that replaces the previous EventEmitter implementation. The change stems from the need to centralize event management through a single, globally accessible entity, which simplifies the handling of events across different components of the application. The EventBus follows the Singleton pattern, ensuring a single instance exists throughout the application lifecycle.
    
    BREAKING CHANGE: All components relying on EventEmitter must now switch to using EventBus. This change requires refactoring in all areas where EventEmitter was previously used.

---
+++ src/models/EventBus.ts
@@ -0,0 +1,32 @@
+import { IEventBus } from "./interfaces/IEventBus";
+import { eventType } from "./interfaces/IEventEmitter";
+
+export class EventBus implements IEventBus {
+  private static instance: EventBus;
+  private listeners: { [event: string]: Function[] } = {};
+
+  private constructor() { } // private constructor to prevent direct construction calls with the 'new' operator.
+
+  public static getInstance(): EventBus {
+    if (!EventBus.instance) {
+      EventBus.instance = new EventBus();
+    }
+
+    return EventBus.instance;
+  }
+
+  subscribe(event: eventType, listener: Function): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  publish(event: eventType, ...args: any[]): void {
+    (this.listeners[event] || []).forEach(listener => listener(...args));
+  }
+
+  clear(): void {
+    this.listeners = {};
+  }
+}
---
+++ src/models/interfaces/IEventBus.ts
@@ -0,0 +1,31 @@
+export type eventType =
+  | 'gameStarted'
+  | "drawInitialCards"
+  | "endTurn"
+  | "nextTurn"                // Includes details about the next player
+  | "playCardOnCaravan"       // Includes details about which caravan with which card
+  | "playCardOnCard"          // Includes details about which card was targeted with which card
+  | "discardCard"             // Includes details about the card discarded
+  | "disbandCaravan"          // Which caravan was disbanded
+  | "updateCaravanBid"        // Which caravan should be updated
+  | "updateCaravansBids"
+  | "playJokerOnAce"          // Details about which ace (suit) and the effect
+  | "playJokerOnNumber"       // Details about which number and the effect
+  | "playJack"                // Details about which card was targeted
+  | "playQueen"               // Details about which card was targeted and its effects (direction and suit change)
+  | "playKing"                // Details about which card was targeted and its effects (value added)
+  | "caravanSold"             // Which caravan was sold and its value
+  | "opponentOutOfCards"
+  | "trackTied"               // When a caravan track reaches a tie
+  | "disbandTiedTrack"        // When a tied caravan track is disbanded
+  | "sabotageOpponentCaravan" // This could be a more general event that occurs when an action specifically targets the opponent's caravan
+  | "gameOver"                // Details about the winner and the final state of caravans
+  | "invalidGameState"        // Details about the invalid game state
+  | "invalidPlay"             // Details about the invalid play
+  ;
+
+export interface IEventBus {
+  subscribe(event: eventType, listener: Function): void;
+  publish(event: eventType, ...args: any[]): void;
+  clear(): void;
+}
