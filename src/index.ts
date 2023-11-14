03668729bfa8cce505ff7a488b0a36326921f3b0
AtilioA <atiliogpg@protonmail.com>
2023-11-14 11:17:23 -0300

    fix: add project entrypoint

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// Constants and Enums
+export * from "./constants/cardConstants";
+export * from "./enums/directions";
+
+// Exceptions
+export * from "./exceptions/GameExceptions";
+
+// Models and their interfaces
+export * from "./models/AI/EasyStrategy";
+export * from "./models/AI/RandomStrategy";
+// export * from './models/Caravan';
+// export * from './models/Card';
+// export * from './models/Deck';
+// export * from './models/EventBus';
+// export * from './models/Game';
+// export * from './models/Player';
+
+// Interfaces
+export * from "./models/interfaces/AIStrategy";
+export * from "./models/interfaces/ICaravan";
+export * from "./models/interfaces/ICard";
+export * from "./models/interfaces/IDeck";
+export * from "./models/interfaces/IEventBus";
+export * from "./models/interfaces/IGame";
+export * from "./models/interfaces/IPlayer";
+
+// Utils
+export * from "./utils/array";
+export * from "./utils/caravan";
+export * from "./utils/card";
